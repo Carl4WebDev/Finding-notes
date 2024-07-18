93e3d37363c512fb397dc7e613ae460979234650
carl Ivan Sampan <carlforwebdev@gmail.com>
2024-07-18 19:05:34 +0600

    timer

--- index.js
+++ index.js
@@ -1,45 +1,58 @@
-const openStringNotes = ["Low E", "A", "D", "G", "B", "High E"]
+const openStringNotes = ["L-E", "A", "D", "G", "B", "H-E"]
+const openStringNotesBass = ["L-E", "A", "D", "G"]
 const musicNotes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
-let start = new Date().getTime();
-let end = new Date().getTime();
-
-function startTimer(){
-    start = new Date().getTime()
+let button = document.getElementById('play-button') 
+let after_button = document.getElementById('after-button') 
+// let selectTypeGuitar = document.getElementById('type-guitar')
+// let selectedValue = selectTypeGuitar.value
+let countdownTime = 4;
+let counter = 0;
+// let resets = 0;
+button.addEventListener('click', function() {    
+    startGame();
+});
+function startGame(){
+    button.style.display = "none";
+    after_button.style.display = "block";
+    setInterval(updator, 1000)
+    updator()
 }
-function endTimer(){
-    end = new Date().getTime()
-}
-
-document.getElementById('play-button').addEventListener('click', function() {
-    startTimer();
+// function resetGame(){
+//     countdownTime = 0
+//     counter = 0
+//     button.style.display = "block";
+//     after_button.style.display = "none";
+// }
+function changeNotes(){
+    // if(selectedValue == )
     let input_1 = document.getElementById('input-1');
     let input_2 = document.getElementById('input-2');
-    let timer = document.getElementById('timer');
     
     input_1.innerText = openStringNotes[Math.floor(Math.random() * openStringNotes.length)]
     input_2.innerText = musicNotes[Math.floor(Math.random() * musicNotes.length)]
-    
-    timer.innerText = (start - end) / 1000
-    endTimer()
+}
+function pad(number) {
+    return number < 10 ? '0' + number : number;
+}
+
+function updator() {
+    if (countdownTime > 0) {
+        countdownTime--;
+        const seconds = pad(countdownTime);
+        document.getElementById('timer').innerText = seconds;
+    }
+    else{
+        if(counter === 2){
+
+            alert("Refresh to play again.")
+        }else {
+            changeNotes()
+            counter++
+            countdownTime = 4;
+        }
+    }
+}
 
 
-});
 
 
-// function pad(number) {
-//     return number < 10 ? '0' + number : number;
-// }
-
-// function updateTimer() {
-//     const now = new Date();
-//     const hours = pad(now.getHours());
-//     const minutes = pad(now.getMinutes());
-//     const seconds = pad(now.getSeconds());
-
-//     document.getElementById('hours').textContent = hours;
-//     document.getElementById('minutes').textContent = minutes;
-//     document.getElementById('seconds').textContent = seconds;
-// }
-
-// setInterval(updateTimer, 1000);
-// updateTimer(); // Initial call to display the time immediately
